a696de3ac9f3ed280e73bbd43fe84a33642fa708
Dyzody <david.schilcher11@gmx.at>
2024-09-20 20:47:13 +0200

    Added more documentation to the music_queue.py

--- music_queue.py
+++ music_queue.py
@@ -6,27 +6,32 @@ class queue:
         self.file_array = self.full_file_array
         self.loop_when_done_playing = loop_when_done_playing
     
+    # Returns the name of the current song in the queue
     def get_current_song(self):
         return self.file_array[0]
     
+    # Restarts the playlist
     def loop(self):
         self.file_array = self.full_file_array
     
+    # Clears the playlist
     def clear(self):
         self.file_array = []
         self.full_file_array = []
 
+    # Goes to the next song OR loops
     def goto_next_song(self):
         del self.file_array[0]
 
         if len(self.file_array) <= 0 and self.loop_when_done_playing:
             self.loop()
 
+    # Converts a song name to a path name
     def name_to_path(self, name):
         return rf"C:\Users\{os.getlogin()}\Music\Discord\{name}"
     
+    # Adds a new song to the queue
     def add_to_queue(self, filenname):
-        
         if not os.path.exists(self.name_to_path(filenname)):
             return f"{filenname} doesn't exist."
 
@@ -34,6 +39,7 @@ class queue:
 
         return f"Successfully added {filenname} to the queue."
 
+    # Returns a string containing every song in the playlist
     def list_queue(self):
         liststr = "Queue:\n"
         for index, item in enumerate(self.file_array):
